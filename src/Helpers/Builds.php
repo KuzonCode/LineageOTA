0f4f0b9815b352d6bded7a7a43c702569025f02b
Bradley <bradley@appenate.com>
2022-01-19 08:28:06 +1000

    Disable Cache with Env Variable

--- src/Helpers/Builds.php
+++ src/Helpers/Builds.php
@@ -140,7 +140,7 @@
                             $build = null;
 
                             // If APC is enabled
-                            if( extension_loaded('apcu') && ini_get('apc.enabled') ) {
+                            if( extension_loaded('apcu') && ini_get('apc.enabled') && is_false($_ENV['DISABLE_CACHE'])) {
                                 $build = apcu_fetch( $file );
 
                                 // If not found there, we have to find it with the old fashion method...
